f5da592ac96ab3a2b4020305592a147ea985ef68
GravisUltrasound <keshyin@yandex.ru>
2020-04-28 21:51:01 +0300

    Tooltip

--- projects/test-lib/src/lib/text-tooltip/text-tooltip/text-tooltip.component.ts
+++ projects/test-lib/src/lib/text-tooltip/text-tooltip/text-tooltip.component.ts
@@ -1,15 +1,37 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'tl-text-tooltip',
   templateUrl: './text-tooltip.component.html',
   styleUrls: ['./text-tooltip.component.css']
 })
-export class TextTooltipComponent implements OnInit {
+export class TextTooltipComponent implements OnInit, AfterViewInit {
   @Input() text: string;
   @Input() textStringNumber: number;
+  @ViewChild('height') nameParagraph: ElementRef;
+  firstTextHeight: number;
+  textHeight: number;
+  ellipsisAreNeed: boolean;
+
   constructor() { }
 
+  ellipsisControl(){
+   this.firstTextHeight = this.nameParagraph.nativeElement.offsetHeight;
+   const textHeight = this.calculateTextHeight();
+   if (textHeight < this.firstTextHeight) {
+     this.textHeight = textHeight;
+     this.ellipsisAreNeed = true;
+   } else {
+     this.textHeight = this.firstTextHeight;
+     this.ellipsisAreNeed = false;
+   }
+  }
+  calculateTextHeight(){
+    return this.textStringNumber * 20;
+  }
+  ngAfterViewInit() {
+    this.ellipsisControl();
+  }
   ngOnInit() {
   }
 
